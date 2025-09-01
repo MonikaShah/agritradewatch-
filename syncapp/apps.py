b876d20d7d9c6a24daccef7e99d8b121d70570cb
monikashah <monikapatira@gmail.com>
2025-09-01 16:08:38 +0530

    Removed Firebase

--- syncapp/apps.py
+++ syncapp/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 import threading
 
-class SyncappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'syncapp'
+# class SyncappConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'syncapp'
 
-    def ready(self):
-        from .firebase_listener import start_listeners
-        threading.Thread(target=start_listeners, daemon=True).start()
+#     def ready(self):
+#         from .firebase_listener import start_listeners
+#         threading.Thread(target=start_listeners, daemon=True).start()
