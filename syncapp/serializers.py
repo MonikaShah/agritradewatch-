82d6815d46e3588ce971300ccae68dd23ef42425
monikashah <monikapatira@gmail.com>
2025-09-02 13:15:29 +0530

    Updted serializers.py

--- syncapp/serializers.py
+++ syncapp/serializers.py
@@ -1,8 +1,29 @@
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
-from .models import Consumer
+from .models import User1, Farmer1, Consumer1, WebData,Consumer
+from rest_framework import serializers   # <-- this import is required
 
 class ConsumerGeoSerializer(GeoFeatureModelSerializer):
     class Meta:
         model = Consumer
         geo_field = "geom"  # your PointField name
         fields = ('id', 'name','data')  # fields to include in properties
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User1
+        fields = '__all__'
+
+class FarmerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Farmer1
+        fields = '__all__'
+
+class ConsumerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Consumer1
+        fields = '__all__'
+
+class WebDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WebData
+        fields = '__all__'
