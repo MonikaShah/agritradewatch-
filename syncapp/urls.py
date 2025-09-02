31c24f78c95e55f251623dbe4a4d888fb752d6db
monikashah <monikapatira@gmail.com>
2025-09-02 12:32:13 +0530

    Modified landing page as map_chart

--- syncapp/urls.py
+++ syncapp/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework import routers
+from .views import UserViewSet, FarmerViewSet, ConsumerViewSet, WebDataViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'farmers', FarmerViewSet)
+router.register(r'consumers', ConsumerViewSet)
+router.register(r'webdata', WebDataViewSet)
 
 urlpatterns = [
     # path('map/', views.consumers_map_view, name='consumers-map'),
@@ -7,11 +15,14 @@ urlpatterns = [
     # path('map_user_products/', views.map_user_products, name='map_user_products'),
     path('consumers-geojson/', views.consumers_geojson, name='consumers-geojson'),
     # path('map_user_products_list/', views.map_user_products_list, name='map_user_products_list'),
-    path('map_chart/', views.map_chart, name='map_chart'),
+    path('', views.map_chart, name='map_chart'),
     path('agrowon_prices/', views.agrowon_prices, name='agrowon_prices'),
     # path('dashboard/',views.dashboard,name='dashboard.html'),
     
     path('api/avg_consumer_price/', views.avg_consumer_price, name='avg_consumer_price'),
 
+    # include DRF router URLs under /api/
+    path('api/', include(router.urls)),
 ]
+
 
