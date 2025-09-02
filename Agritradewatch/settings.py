9351edf1125bb4c546bc5f92c38eb328e7496840
monikashah <monikapatira@gmail.com>
2025-09-02 14:10:02 +0530

    Added schema public in db

--- Agritradewatch/settings.py
+++ Agritradewatch/settings.py
@@ -89,6 +89,8 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'options': '-c search_path=public'
     }
 }
 
