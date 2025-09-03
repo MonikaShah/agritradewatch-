9939d961e1ce527ed65f391be8579d301cfb0667
monikashah <monikapatira@gmail.com>
2025-09-03 14:35:01 +0530

    setting updted

--- Agritradewatch/settings.py
+++ Agritradewatch/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'syncapp',
     'django.contrib.gis',
     'rest_framework',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'Agritradewatch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
