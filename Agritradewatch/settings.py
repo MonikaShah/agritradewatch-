c2365e807cd62eea97620094202f404ed148ff62
monikashah <monikapatira@gmail.com>
2025-11-20 18:27:47 +0530

    setting debug false

--- Agritradewatch/settings.py
+++ Agritradewatch/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2acdn_!1^g!f^3w4mwn04y^w7i0vk19*_ox5ewc+^y^2vgvkz+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [config('ALLOWED_HOST_1'), config('ALLOWED_HOST_2'),'localhost',
     '127.0.0.1']
@@ -199,8 +199,8 @@ EMAIL_HOST_USER = config('EMAIL_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # 
-#FRONTEND_URL = 'http://127.0.0.1:8000'
-FRONTEND_URL= 'https://mandigo.in'
+FRONTEND_URL = 'http://127.0.0.1:8000'
+# FRONTEND_URL= 'https://mandigo.in'
 
 
 USE_I18N = True
