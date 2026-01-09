6103e7f2a8bf72c6f1e80fef899570afd36ca1c6
monikashah <monikapatira@gmail.com>
2026-01-09 15:52:12 +0530

    udated GA

--- Agritradewatch/utils/ga4.py
+++ Agritradewatch/utils/ga4.py
@@ -1,19 +1,14 @@
+import os
+from django.conf import settings
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import DateRange, Metric, RunReportRequest
 from google.oauth2 import service_account
 
-# ✅ ABSOLUTE PATH (Apache safe)
-GA_CREDENTIALS = "/home/agritradewatch/Agritradewatch/credentials/ga-service-account.json"
-
-# ⚠️ GA4 PROPERTY ID (numbers only)
+GA_CREDENTIALS = os.path.join(settings.BASE_DIR, "credentials/ga-service-account.json")
 GA4_PROPERTY_ID = "519107243"
 
-credentials = service_account.Credentials.from_service_account_file(
-    GA_CREDENTIALS
-)
-
+credentials = service_account.Credentials.from_service_account_file(GA_CREDENTIALS)
 client = BetaAnalyticsDataClient(credentials=credentials)
-
 
 def get_total_users():
     """
@@ -24,6 +19,5 @@ def get_total_users():
         date_ranges=[DateRange(start_date="2021-01-01", end_date="yesterday")],
         metrics=[Metric(name="totalUsers")],
     )
-
     response = client.run_report(request)
     return int(response.rows[0].metric_values[0].value)
