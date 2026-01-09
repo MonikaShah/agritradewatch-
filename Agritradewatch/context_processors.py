3ca287898f8ec116d2b66247c77b7d2c42b75cde
monikashah <monikapatira@gmail.com>
2026-01-09 14:42:35 +0530

    updated contect processors.py

--- Agritradewatch/context_processors.py
+++ Agritradewatch/context_processors.py
@@ -4,11 +4,12 @@ from Agritradewatch.utils.ga import get_total_users
 logger = logging.getLogger(__name__)
 
 def analytics_context(request):
-    try:
-        visitors = get_total_users()
-    except Exception as e:
-        logger.error(f"GA fetch failed: {e}")
-        visitors = 0
+    visitors = get_total_users() 
+    # try:
+    #     visitors = get_total_users()
+    # except Exception as e:
+    #     logger.error(f"GA fetch failed: {e}")
+    #     visitors = 0
 
     return {"TOTAL_VISITORS": visitors}
 
