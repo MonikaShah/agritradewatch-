b4893468406fb7ca6500b5bebe44f73601241c45
monikashah <monikapatira@gmail.com>
2026-01-09 17:18:53 +0530

    Updated GA

--- Agritradewatch/context_processors.py
+++ Agritradewatch/context_processors.py
@@ -1,16 +1,8 @@
 # Agritradewatch/context_processors.py
 from django.core.cache import cache
-
-# Safe import of GA function
-try:
-    from .ga4 import get_total_users
-except ImportError:
-    # fallback if ga4.py is missing
-    get_total_users = lambda: 0
-    print("GA4 import failed: using dummy get_total_users function")
+from Agritradewatch.utils.ga4 import get_total_users
 
 def analytics_context(request):
-    print("DEBUG: analytics_context running")
     """
     Adds GA total users to template context for footer.
     Cached for 5 minutes.
@@ -19,11 +11,8 @@ def analytics_context(request):
     if total is None:
         try:
             total = get_total_users()
-            print("GA total users:", total)  # debug output in console
         except Exception as e:
             print("GA fetch failed:", e)
-            total = 0  # fallback if GA API fails
+            total = 0
         cache.set("total_visitors", total, 300)  # cache 5 minutes
-    print("DEBUG: get_total_users output =", total)
-    
     return {"total_visitors": total}
